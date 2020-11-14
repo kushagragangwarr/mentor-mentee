6fa953abd66f66577e5c5008b48bf61894242de1
unknown <kushagragangwar4@gmail.com>
2020-11-14 09:57:43 +0530

    Updated links for redirecting

--- js/login.js
+++ js/login.js
@@ -5,11 +5,37 @@ const loginSubmitBtn = document.querySelector("#login-submit-btn");
 
 loginSubmitBtn.addEventListener("click", e => {
     e.preventDefault();
+    if (nameInput.value === "" || passInput.value === "") 
+        return;
+
     let accounts = localStorage.getItem("accounts");
-    if (accounts === null) return;
+    if (accounts === null) {
+        userAlert();
+        return;
+    }
     
     accounts = JSON.parse(accounts);
-    if ((accounts.name === nameInput.value || accounts.email === nameInput.value) && accounts.pass === passInput.value)
-        location.assign('../main.html');
-    else console.log("NO");
+    if (accounts.name === nameInput.value || accounts.email === nameInput.value) {
+        if (accounts.pass === passInput.value)
+            location.assign("../mentor-mentee/main.html");
+        else passAlert();
+    } else userAlert();
 });
+
+function userAlert() {
+    const noUserAlert = document.querySelector("#no-user-alert");
+    noUserAlert.style.display = "block";
+    window.setTimeout(function() {
+        noUserAlert.style.display = "none";
+    }, 2000);
+    return;
+}
+
+function passAlert() {
+    const wrongPassAlert = document.querySelector("#wrong-pass-alert");
+    wrongPassAlert.style.display = "block";
+    window.setTimeout(function() {
+        wrongPassAlert.style.display = "none";
+    }, 2000);
+    return;
+}
