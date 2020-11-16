ba74be3830301e59d61220cd42e530bb04cf0471
unknown <kushagragangwar4@gmail.com>
2020-11-16 16:05:52 +0530

    Added sidebar toggler and updated info on cards

--- js/main.js
+++ js/main.js
@@ -1,18 +1,67 @@
 // All declarations and initializations
-const logoutBtn = document.querySelector("#logout-btn");
+const logoutBtns = document.querySelectorAll(".logout");
+const filterLabels = document.querySelectorAll(".custom-control-label");
+const detailsDivs = document.querySelectorAll(".details");
+const sidebar = document.querySelector("#sidebar");
+const toggler = document.querySelector("#toggler");
+const filterArray = [];
 
+// Setting the routes
 if (localStorage.getItem("login") === null) {
     // location.assign('../error.html');
     location.assign('../../mentor-mentee/error.html');
 }
 
-logoutBtn.addEventListener("click", e => {
-    e.preventDefault();
-    localStorage.removeItem("login");
-    // location.assign("../index.html");
-    location.assign("../../mentor-mentee/index.html");
+logoutBtns.forEach(logoutBtn => {
+    logoutBtn.addEventListener("click", e => {
+        e.preventDefault();
+        localStorage.removeItem("login");
+        // location.assign("../index.html");
+        location.assign("../../mentor-mentee/index.html");
+    });
 });
 
+// For applying filters
+filterLabels.forEach(filterLabel => {
+    filterLabel.addEventListener("click", checkFilter);
+});
+
+// Enabling tooltip
 $(function () {
-    $('[data-toggle="tooltip"]').tooltip()
-})
+    $('[data-toggle="tooltip"]').tooltip();
+});
+
+// Sidebar toggler
+toggler.addEventListener("click", checkSidebar);
+
+
+// Function for checking the filter and displaying data accordingly
+
+function checkFilter(e) {
+    const filterID = e.target.parentNode.childNodes[1].id;
+    if (filterArray.includes(filterID)) {
+        var index = filterArray.indexOf(filterID);
+        filterArray.splice(index, 1);
+    } else {
+        filterArray.push(filterID);
+    }
+    detailsDivs.forEach(detailsDiv => {
+        detailsDiv.parentNode.classList.remove("hide");
+        if (filterArray.length != 0) {
+            filterArray.forEach(filter => {
+                if (!detailsDiv.classList.contains(filter))
+                    detailsDiv.parentNode.classList.add("hide");
+            });
+        }
+    });
+}
+
+function checkSidebar() {
+    if (sidebar.classList.contains("right100")) {
+        sidebar.classList.remove("right100");
+        sidebar.classList.add("right0");
+    } else {
+        sidebar.classList.remove("right0");
+        sidebar.classList.add("right100");
+    }
+}
